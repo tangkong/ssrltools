bbb65f90645589a87539e452c65620cb066746e0
tangkong <tangkongrobert@gmail.com>
2020-01-28 21:07:22 -0800

    Added debugging callbacks

--- ssrltools/callbacks.py
+++ ssrltools/callbacks.py
@@ -8,14 +8,21 @@ Callbacks for use at SSRL beamlines running Bluesky
 
 from bluesky.callbacks import CallbackBase
 
-class ownCB(CallbackBase):
+class own_CB(CallbackBase):
     def start(self, doc):
-        print('start of run')
+        print('>>> start of run <<<  \n')
     
     def event(self, doc):
         #print('name: {}'.format(name))
-        print('data: {}'.format(doc['data']))
+        print('>>> data: {} <<< '.format(doc['data']))
         
     def stop(self, doc):
-        print('run completed')
+        print('>>> run completed <<<')
         
+def doc_contents(key, doc):
+    """
+    prints document contents -- use for diagnosing a document stream
+    """
+    print(key + ' ==========================')
+    for k, v in doc.items():
+        print(f"\t{k}\t{v}")
