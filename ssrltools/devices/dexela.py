bb109f7a86415853e6bfd828ad579f306efdb7eb
tangkong <tangkongrobert@gmail.com>
2020-06-25 12:47:28 -0700

    updated dexela config

--- ssrltools/devices/dexela.py
+++ ssrltools/devices/dexela.py
@@ -22,7 +22,7 @@ from ophyd import Signal, CamBase
 from ophyd import AreaDetector, SingleTrigger
 
 from ophyd.signal import EpicsSignal, EpicsSignalBase
-from ophyd.areadetector import EpicsSignalWithRBV as SignalWithRBV
+from ophyd.areadetector import cam, (ADComponent as ADCpt), (EpicsSignalWithRBV as SignalWithRBV), 
 from ophyd.areadetector.detectors import DetectorBase, DexelaDetector
 from ophyd.areadetector.filestore_mixins import (resource_factory, 
                                             FileStoreTIFFIterativeWrite)
@@ -36,3 +36,25 @@ class SSRLDexelaDet(SingleTrigger, DexelaDetector):
     Contains main PV's
     """
     pass
+
+class DexelaDet15(SSRLDexelaDet):
+    """
+    Final class for Dexela Detector on SSRL BL 1-5
+    - add Plugins (TIFF plugin, etc)
+    - set up read, configuration attrs
+
+    det = DexelaDet15(prefix, name='name', read_attrs=['attr'])
+    """
+    # DexelaDetector from ophyd pulls in all Dexela specific PV's
+    write_path = 'C:\\dexela_images\\tmp'
+    cam = ADC(cam.DexelaDetectorCam, '')
+    # In case where TIFF plugin is being used
+    tiff = Cpt(DexelaTiffPlugin, 'TIFF:',
+                       write_path_template=write_path,
+                       read_path_template=write_path,
+                       path_semantics='windows')
+    # Else there should be an NDArrayData PV
+    image = Cpt(EpicsSignal, 'IMAGE1:ArrayData')
+
+    # Could add more attributes to file_plugin
+    # could add stage behavior
