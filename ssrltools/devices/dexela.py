8562a5388e02bee41864e2d4dcf7b0c0848cbfe4
tangkong <tangkongrobert@gmail.com>
2020-07-21 14:22:49 -0700

    Fix Dexela port names issue due to IOC mis-config

--- ssrltools/devices/dexela.py
+++ ssrltools/devices/dexela.py
@@ -18,7 +18,7 @@ import uuid
 import threading
 
 from ophyd import Device, Component as Cpt, DeviceStatus
-from ophyd import Signal, CamBase
+from ophyd import SignalRO, CamBase
 from ophyd import AreaDetector, SingleTrigger
 
 from ophyd.signal import EpicsSignal, EpicsSignalBase
@@ -37,6 +37,9 @@ class SSRLDexelaDet(SingleTrigger, DexelaDetector):
     """
     pass
 
+class HackedCam(cam.DexelaDetectorCam):
+    port_name = Cpt(SignalRO, value='DEX1')
+
 class DexelaDet15(SSRLDexelaDet):
     """
     Final class for Dexela Detector on SSRL BL 1-5
@@ -46,15 +49,20 @@ class DexelaDet15(SSRLDexelaDet):
     det = DexelaDet15(prefix, name='name', read_attrs=['attr'])
     """
     # DexelaDetector from ophyd pulls in all Dexela specific PV's
-    write_path = 'C:\\dexela_images\\tmp'
-    cam = ADC(cam.DexelaDetectorCam, '')
+    write_path = 'E:\\dexela_images\\'
+
+    # Hack to get around EPICS strangeness. Port_name and nd_array_ports
+    # don't match...
+    cam = ADCpt(HackedCam, '')
+
     # In case where TIFF plugin is being used
     tiff = Cpt(DexelaTiffPlugin, 'TIFF:',
                        write_path_template=write_path,
-                       read_path_template=write_path,
+                       read_path_template='/dexela_images/',
                        path_semantics='windows')
     # Else there should be an NDArrayData PV
     image = Cpt(EpicsSignal, 'IMAGE1:ArrayData')
+    highest_pixel = Cpt(EpicsSignal, 'HighestPixel')
 
     # Could add more attributes to file_plugin
-    # could add stage behavior
+    # could add stage behavior, maybe to coerce pv's?
