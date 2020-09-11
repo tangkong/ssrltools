88206e9e312e00465ddb6781f6b4aad9c1c6f896
tangkong <tangkongrobert@gmail.com>
2020-09-11 15:14:37 -0700

    MarCCD IOC workarounds

--- ssrltools/devices/areadetectors.py
+++ ssrltools/devices/areadetectors.py
@@ -16,12 +16,14 @@ from ophyd import Device, Component as Cpt, DeviceStatus, ADComponent as ADCpt
 from ophyd import CamBase
 from ophyd import AreaDetector, SingleTrigger
 
-from ophyd.signal import SignalRO, EpicsSignal, EpicsSignalBase
+from ophyd.base import ADBase
+from ophyd.signal import SignalRO, EpicsSignal, EpicsSignalBase, Signal
 from ophyd.areadetector import EpicsSignalWithRBV as SignalWithRBV
 from ophyd.areadetector.detectors import (DetectorBase, DexelaDetector, 
                                             PilatusDetector, MarCCDDetector)
 from ophyd.areadetector.filestore_mixins import (resource_factory, 
-                                            FileStoreTIFFIterativeWrite)
+                                            FileStoreTIFFIterativeWrite,
+                                            FileStorePluginBase)
 from ophyd.areadetector import TIFFPlugin, cam
 
 class DexelaTiffPlugin(TIFFPlugin, FileStoreTIFFIterativeWrite):
@@ -81,6 +83,77 @@ class PilatusDet15(SingleTrigger, PilatusDetector):
                 read_path_template='/pilatus_images/',
                 path_semantics='windows')
                 
+class MarTiffFakePlugin(ADBase, GenerateDatumInterface):
+    """
+    Connects to subset of PV's present in MarCCD, without the plugin-specific pv
+    Look to stage sigs etc for which are necessary
+
+    Hacking to connect to PV's with the same class structure, but without the
+    plugin machinery
+
+    Commented lines denote PV's that don't exist in this IOC
+    """
+    _default_suffix = ''
+    # _html_docs = ['NDPluginFile.html']
+    # _plugin_type = 'NDPluginFile'
+    FileWriteMode = enum(SINGLE=0, CAPTURE=1, STREAM=2)
+
+    auto_increment = Cpt(SignalWithRBV, 'AutoIncrement', kind='config')
+    auto_save = Cpt(SignalWithRBV, 'AutoSave', kind='config')
+    capture = Cpt(SignalWithRBV, 'Capture')
+    delete_driver_file = Cpt(SignalWithRBV, 'DeleteDriverFile', kind='config')
+    file_format = Cpt(SignalWithRBV, 'FileFormat', kind='config')
+    file_name = Cpt(SignalWithRBV, 'FileName', string=True, kind='config')
+    file_number = Cpt(SignalWithRBV, 'FileNumber')
+    # file_number_sync = Cpt(EpicsSignal, 'FileNumber_Sync')
+    file_number_write = Cpt(EpicsSignal, 'FileNumber_write')
+    file_path = Cpt(SignalWithRBV, 'FilePath', string=True, kind='config')
+    file_path_exists = Cpt(EpicsSignalRO, 'FilePathExists_RBV', kind='config')
+    file_template = Cpt(SignalWithRBV, 'FileTemplate', string=True, kind='config')
+    file_write_mode = Cpt(SignalWithRBV, 'FileWriteMode', kind='config')
+    full_file_name = Cpt(EpicsSignalRO, 'FullFileName_RBV', string=True, kind='config')
+    num_capture = Cpt(SignalWithRBV, 'NumCapture', kind='config')
+    num_captured = Cpt(EpicsSignalRO, 'NumCaptured_RBV')
+    read_file = Cpt(SignalWithRBV, 'ReadFile')
+    write_file = Cpt(SignalWithRBV, 'WriteFile')
+    write_message = Cpt(EpicsSignal, 'WriteMessage', string=True)
+    write_status = Cpt(EpicsSignal, 'WriteStatus')
+
+class MarFileStoreTIFF(FileStoreTIFFIterativeWrite, MarTiffFakePlugin):
+    """
+    Mostly mimicking FileStoreTIFF, and Xspress3FileStore
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.stage_sigs.update([('file_template', '%s%s_%6.6d.tiff'),
+        #                         ('file_write_mode', 'Single'),
+        #                         ])
+
+        # 'Single' file mode means one image : one file
+        # It does NOT mean 'num_images' is ignored
+
+        # remove array_counter
+        self.stage_sigs.pop('array_counter')
+
+        # # add filestore_spec
+        # self.filestore_spec = 'AD_TIFF'
+
+        # add enable signal that's missing from lack of real plugin...?
+        # checked by trigger dispatching
+        self.enable = Signal(name='Enable', value='1')
+
+    # def get_frames_per_point(self):
+    #     return self.parent.cam.num_images.get()
+
+    # def stage(self):
+    #     super().stage()  ## apply stage sigs (BlueskyInterface.stage())
+
+    #     self._fn = self._fp
+
+    #     resource_kwargs = {'template': self.file_template.get(),
+    #                         'filename': self.file_name.get(),
+    #                         'frame_per_point': self.get_frames_per_point()}
+    #     self._generate_resource(resource_kwargs)
 
 class MarCCDDet15(SingleTrigger, MarCCDDetector):
     """MarCCDDet15 
